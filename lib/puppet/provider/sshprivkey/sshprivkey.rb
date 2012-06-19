a3178c42f82d7221aeb25293468d5948a38df279
Ben Hughes <git@mumble.org.uk>
2012-06-18 17:18:34 -0700

    Initial commit of sshprivkey type/provider.

---
+++ lib/puppet/provider/sshprivkey/sshprivkey.rb
@@ -0,0 +1,61 @@
+require 'fileutils'
+require 'pp'
+
+Puppet::Type.type(:sshprivkey).provide(:sshprivkey) do
+
+  desc 'provider for SSH private keys.'
+
+  commands :keygen => "/usr/bin/ssh-keygen"
+
+  def create
+    begin
+      Puppet::Util.execute( [ '/usr/bin/ssh-keygen' , '-q', '-N', '', '-f', @resource[:path] ] , :uid => @resource[:user] )
+    rescue => e
+      Puppet.warning "ssh-keygen failed in a bad way. '#{e}'"
+      return false
+    end
+
+    # This is sketchy, just mailing around the key, but what else should
+    # one do?
+    if @resource[:mailto] != nil
+
+      unless File.exists? "#{@resource[:path]}.pub"
+        Puppet.warning "So I should have had a SSH public key, but I now don't"
+        raise Puppet::Error
+      end
+
+      begin
+        # hack hack hack
+        system( "mail -s 'Puppet generated an SSH key for you.' #{@resource[:mailto]} <#{@resource[:path]}.pub" )
+      rescue => e
+        Puppet.warning "I broke the emails, sending you a stolen SSH key."
+      end
+    end
+
+  end
+
+  def destroy
+    if FileUtils.rm( @resource[:path], :force => true ) and
+      FileUtils.rm( "#{@resource[:path]}.pub", :force => true )
+      return true
+    else
+      return false
+    end
+  end
+
+  def exists?
+    return false unless File.exists? @resource[:path]
+
+    File.open( @resource[:path], 'r' ) do |f|
+      f.readlines.each do |keyline|
+        return true if keyline =~ /-----BEGIN [RD]SA PRIVATE KEY-----/
+      end
+    end
+    return false
+  end
+
+#  autorequire(:user) do
+#    should(:user) if should(:user)
+#  end
+
+end
---
+++ lib/puppet/type/sshprivkey.rb
@@ -0,0 +1,47 @@
+Puppet::Type.newtype(:sshprivkey) do
+  desc 'manage SSH private keys.'
+
+  ensurable
+
+  newparam(:user) do
+    desc 'user for whom the SSH key is to be made.'
+
+    defaultto 'root'
+
+    validate do |value|
+      unless value =~ /^\w+$/
+        raise ArgumentError, "username is not valid: #{value}"
+      end
+    end
+
+  end
+
+  newparam(:path, :namevar => true ) do
+    desc 'filepath where the SSH key is to be placed.'
+
+    validate do |value|
+      unless value =~ /^\/[\w\/]+$/
+        raise ArgumentError, "file path is not valid: #{value}"
+      end
+
+      if value =~ /\.pub$/
+        raise ArgumentError, "file path states public key, not private key: #{value}"
+      end
+    end
+
+  end
+
+  newparam(:mailto) do
+    desc 'An email address to mail the public key to'
+    validate do |value|
+      unless value =~ /[\w+\.\!\+]+@[\w\.]+/
+        raise ArgumentError, "Invalid email address: #{value}"
+      end
+    end
+  end
+
+  #autorequire(:user) do
+  #  should(:user) if should(:user)
+  #end
+
+end
